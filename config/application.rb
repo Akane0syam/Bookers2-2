3ac74c089a97e8aee46232cb952a1f3fd8d17fee
Akane0syam <11kangae096@gmail.com>
2022-04-24 13:03:58 +0000

    [Add]13まで

---
+++ config/application.rb
@@ -0,0 +1,23 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Bookers2
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "c596d6fa479e4201942efa7dc6c5c147.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  end
+end
