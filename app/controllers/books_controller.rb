5dba48523fef7e3685ae428181477d47daf0e762
Akane0syam <11kangae096@gmail.com>
2022-05-05 14:06:37 +0000

    [Add]userのeditページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,9 +19,18 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = current_user
+    @users = User.all
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
-  def show
-     @user = User.find(params[:id])
-     @book = Book.new
-    @books = Book.where(user_id: @user.id)
-  end
 
   def index
     @user = current_user
     @users = User.all
     @book = Book.new
     @books = Book.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @user = current_user
+    @users = User.all
+    @book = Book.new
+     @books = Book.all
   end
 
   def edit
