1369840cfe34831fbc27431646b76f3f83bd11da
Akane0syam <11kangae096@gmail.com>
2022-05-13 23:40:42 +0000

    [Add]テスト

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module Bookers2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.hosts << "c596d6fa479e4201942efa7dc6c5c147.vfs.cloud9.ap-northeast-1.amazonaws.com"
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,8 +70,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-   config.active_job.queue_adapter = :inline
+  config.active_job.queue_adapter = :inline
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "c596d6fa479e4201942efa7dc6c5c147.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
